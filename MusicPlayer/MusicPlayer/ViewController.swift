d3380099ccce70b36289ace8dd415dcc54f1fb6d
wi <duse34@hotmail.com>
2020-04-10 02:10:49 +0900

    업로드.

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 위대연 on 2020/04/09.
+//  Copyright © 2020 위대연. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var playPauseButton : UIButton!
+    @IBOutlet weak var timeLabel : UILabel!
+    @IBOutlet weak var progressSlider : UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
